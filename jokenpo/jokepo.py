786824eaafbe4589ec6b61d085f835f87023ee76
dodja12 <78220879+dodja12@users.noreply.github.com>
2021-07-05 07:09:48 -0300

    5

--- jokenpo/jokepo.py
+++ jokenpo/jokepo.py
@@ -1,2 +1,10 @@
 from random import randint
 import emoji
+from time import sleep
+rsp =  (emoji.emojize(':new_moon:'), (emoji.emojize(':page_facing_up:')), (emoji.emojize(':scissors:')))
+pc = randint(0,2)
+print('[options]')
+print(emoji.emojize('[ 0 ]:new_moon:'))
+print(emoji.emojize('[ 1 ]:page_facing_up:'))
+print(emoji.emojize('[ 2 ]:scissors:'))
+player = int(input('choice one: '))
